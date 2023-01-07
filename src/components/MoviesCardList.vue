eb26e490e5b54eb902ff2cdf0525807e78bf51c9
tomms25 <tommasobaccano@gmail.com>
2023-01-07 12:42:53 +0100

    methods

---
+++ src/components/MoviesCardList.vue
@@ -0,0 +1,30 @@
+<script >
+import { store } from '../store.js';
+export default {
+  data() {
+    return {
+      active: {},
+      store,
+    }
+  },
+  methods: {
+    // metodo per visualizzare bandiere in base alla lingua
+    displayLanguage(language) {
+      if (language === 'en') {
+        return '<img class="flag" src="https://flagcdn.com/16x12/gb.png" alt="uk-flag" />';
+      }
+      else if (language === 'it') {
+        return '<img class="flag" src="https://flagcdn.com/16x12/it.png" alt="it-flag" />';
+      }
+      else {
+        return language;
+      }
+    },
+    rating(voteAverage) {
+      let vote = Math.round(voteAverage / 2);
+      return vote;
+    }
+  }
+}
+</script>
+
